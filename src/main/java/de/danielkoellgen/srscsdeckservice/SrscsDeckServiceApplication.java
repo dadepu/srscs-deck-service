b712764f836e43657d927caeb4c300e157e3327d
dadepu <danielkoellgen@gmail.com>
2022-02-26 21:38:46 +0100

    initial

---
+++ src/main/java/de/danielkoellgen/srscsdeckservice/SrscsDeckServiceApplication.java
@@ -0,0 +1,13 @@
+package de.danielkoellgen.srscsdeckservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SrscsDeckServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SrscsDeckServiceApplication.class, args);
+    }
+
+}
