438ec308f125845e45e81dc5aaaa3969a4f7cf9d
dadepu <danielkoellgen@gmail.com>
2022-03-24 15:08:19 +0100

    feat(user): allow to create new users

---
+++ src/main/java/de/danielkoellgen/srscsdeckservice/domain/user/application/UserService.java
@@ -0,0 +1,37 @@
+package de.danielkoellgen.srscsdeckservice.domain.user.application;
+
+import de.danielkoellgen.srscsdeckservice.domain.domainprimitive.Username;
+import de.danielkoellgen.srscsdeckservice.domain.user.domain.User;
+import de.danielkoellgen.srscsdeckservice.domain.user.repository.UserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    private final Logger logger = LoggerFactory.getLogger(UserService.class);
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public void addNewExternallyCreatedUser(UUID transactionId, UUID userId, Username username) {
+        User user = new User(userId, username);
+        userRepository.save(user);
+
+        logger.info("New User {} added. [tid={}, userId={}]",
+                user.getUsername(), transactionId, user.getUserId());
+        logger.info("New User added. [{}]", user);
+    }
+
+    public void disableExternallyDisabledUser(UUID transactionId, UUID userId) {
+        throw new RuntimeException("Method not implemented yet.");
+    }
+}
