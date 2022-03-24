7eadebff3306ab379698de0e9e622196d26556eb
dadepu <danielkoellgen@gmail.com>
2022-03-24 12:50:29 +0100

    feat(domainprimitive): add username-primitive implementation

---
+++ src/main/java/de/danielkoellgen/srscsdeckservice/domain/domainprimitive/UsernameException.java
@@ -0,0 +1,7 @@
+package de.danielkoellgen.srscsdeckservice.domain.domainprimitive;
+
+public class UsernameException extends Exception {
+    public UsernameException(String message) {
+        super(message);
+    }
+}
