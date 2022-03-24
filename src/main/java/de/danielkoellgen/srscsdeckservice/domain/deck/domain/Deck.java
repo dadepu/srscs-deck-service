586f7dbcb58acf26d2eacb218958f5cd4ea88b3d
dadepu <danielkoellgen@gmail.com>
2022-03-24 16:02:01 +0100

    feat(deck): add Deck-Entity

---
+++ src/main/java/de/danielkoellgen/srscsdeckservice/domain/deck/domain/Deck.java
@@ -0,0 +1,40 @@
+package de.danielkoellgen.srscsdeckservice.domain.deck.domain;
+
+import de.danielkoellgen.srscsdeckservice.domain.domainprimitive.DeckName;
+import de.danielkoellgen.srscsdeckservice.domain.user.domain.User;
+import lombok.Getter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.PersistenceConstructor;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.DocumentReference;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+import java.util.UUID;
+
+@Getter
+@Document("decks")
+public class Deck {
+
+    @Id
+    private final UUID deckId;
+
+    @Field("deck_name")
+    private final DeckName deckName;
+
+    @DocumentReference
+    @Field("user_id")
+    private final User user;
+
+    public Deck(User user, DeckName deckName) {
+        this.deckId = UUID.randomUUID();
+        this.deckName = deckName;
+        this.user = user;
+    }
+
+    @PersistenceConstructor
+    public Deck(UUID deckId, DeckName deckName, User user) {
+        this.deckId = deckId;
+        this.deckName = deckName;
+        this.user = user;
+    }
+}
