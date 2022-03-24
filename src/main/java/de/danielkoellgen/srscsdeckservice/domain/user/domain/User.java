296bba63cfa1cacaba06062ceb1b1f6cb9751bc1
dadepu <danielkoellgen@gmail.com>
2022-03-24 12:56:42 +0100

    feat(user): add user-entity implementation

---
+++ src/main/java/de/danielkoellgen/srscsdeckservice/domain/user/domain/User.java
@@ -0,0 +1,27 @@
+package de.danielkoellgen.srscsdeckservice.domain.user.domain;
+
+import de.danielkoellgen.srscsdeckservice.domain.domainprimitive.Username;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.UUID;
+
+@Getter
+@AllArgsConstructor
+public class User {
+
+    private UUID userId;
+
+    private Username username;
+
+    private Boolean isActive;
+
+    public User(UUID userId, Username username) {
+        this.userId = userId;
+        this.username = username;
+    }
+
+    public void disableUser() {
+        isActive = false;
+    }
+}
