d68c899d91889e39f0f741593078dc750a4ed6dc
dadepu <danielkoellgen@gmail.com>
2022-03-24 15:58:57 +0100

    feat(domainprimitive): add deck-name primitive

---
+++ src/main/java/de/danielkoellgen/srscsdeckservice/domain/domainprimitive/DeckNameException.java
@@ -0,0 +1,8 @@
+package de.danielkoellgen.srscsdeckservice.domain.domainprimitive;
+
+public class DeckNameException extends Exception {
+
+    public DeckNameException(String message) {
+        super(message);
+    }
+}
