c6fa4b9a10185e115df2420c171e76e8e025c59a
dadepu <danielkoellgen@gmail.com>
2022-03-25 12:33:18 +0100

    feat(card): add ReviewCount primitive

---
+++ src/main/java/de/danielkoellgen/srscsdeckservice/domain/card/domain/ReviewCount.java
@@ -0,0 +1,21 @@
+package de.danielkoellgen.srscsdeckservice.domain.card.domain;
+
+import lombok.Getter;
+
+public class ReviewCount {
+
+    @Getter
+    public final Integer reviewCount;
+
+    private ReviewCount(Integer reviewCount) {
+        this.reviewCount = reviewCount;
+    }
+
+    public static ReviewCount makeDefaultCount() {
+        return new ReviewCount(0);
+    }
+
+    public ReviewCount incrementedCount() {
+        return new ReviewCount(reviewCount + 1);
+    }
+}
