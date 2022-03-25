66aa085c706c5dd782b81862c1992dd51df1fd41
dadepu <danielkoellgen@gmail.com>
2022-03-25 13:37:10 +0100

    feat(schedulerpreset): add MinimumInterval primitive

---
+++ src/main/java/de/danielkoellgen/srscsdeckservice/domain/schedulerpreset/domain/MinimumInterval.java
@@ -0,0 +1,35 @@
+package de.danielkoellgen.srscsdeckservice.domain.schedulerpreset.domain;
+
+import lombok.Getter;
+import org.jetbrains.annotations.NotNull;
+
+import java.time.Duration;
+
+public class MinimumInterval {
+
+    @Getter
+    @NotNull
+    private final Duration minimumInterval;
+
+    private static final Duration minimum = Duration.ofHours(1);
+
+    private MinimumInterval(@NotNull Duration minimumInterval) {
+        validateIntervalOrThrow(minimumInterval);
+        this.minimumInterval = minimumInterval;
+    }
+
+    public static MinimumInterval makeFromDefault() {
+        return new MinimumInterval(minimum);
+    }
+
+    public static MinimumInterval makeFromDuration(@NotNull Duration minimumInterval) {
+        return new MinimumInterval(minimumInterval);
+    }
+
+    private static void validateIntervalOrThrow(@NotNull Duration interval) {
+        if (interval.toMinutes() < minimum.toMinutes()) {
+            throw new IllegalArgumentException(
+                    "MinimumInterval is not supposed to fall below "+ minimum.toMinutes() +" minutes.");
+        }
+    }
+}
