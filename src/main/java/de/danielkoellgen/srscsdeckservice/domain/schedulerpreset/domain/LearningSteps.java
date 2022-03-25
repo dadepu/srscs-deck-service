95f0d30d3c907d612f9311be2175d042a2edd1aa
dadepu <danielkoellgen@gmail.com>
2022-03-25 13:21:14 +0100

    feat(schedulerpreset): add LearningSteps primitive

---
+++ src/main/java/de/danielkoellgen/srscsdeckservice/domain/schedulerpreset/domain/LearningSteps.java
@@ -0,0 +1,35 @@
+package de.danielkoellgen.srscsdeckservice.domain.schedulerpreset.domain;
+
+import lombok.Getter;
+import org.jetbrains.annotations.NotNull;
+
+import java.time.Duration;
+import java.util.List;
+
+public class LearningSteps {
+
+    @Getter
+    @NotNull
+    private final List<Duration> learningStepsInSeconds;
+
+    private static final List<Duration> defaultVal = List.of(Duration.ofMinutes(10));
+
+    private LearningSteps(@NotNull List<Duration> steps) {
+        validateStepsOrThrow(steps);
+        this.learningStepsInSeconds = steps;
+    }
+
+    public static LearningSteps makeDefaultSteps() {
+        return new LearningSteps(defaultVal);
+    }
+
+    public static LearningSteps makeFromListOfDurations(@NotNull List<Duration> steps) {
+        return new LearningSteps(steps);
+    }
+
+    private void validateStepsOrThrow(List<Duration> steps) {
+        if (steps.isEmpty()) {
+            throw new IllegalArgumentException("Must contain at least a single step.");
+        }
+    }
+}
