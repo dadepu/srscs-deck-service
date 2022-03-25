484437bd920289be149f9b96258b93fef0fcab91
dadepu <danielkoellgen@gmail.com>
2022-03-25 12:14:59 +0100

    feat(card): add EaseFactor primitive

---
+++ src/main/java/de/danielkoellgen/srscsdeckservice/domain/card/domain/EaseFactor.java
@@ -0,0 +1,31 @@
+package de.danielkoellgen.srscsdeckservice.domain.card.domain;
+
+import lombok.Getter;
+
+public class EaseFactor {
+
+    @Getter
+    private final Double easeFactor;
+
+    private static final Double minimum = 1.0;
+
+    private static final Double maximum = 3.0;
+
+    private static final Double defaultVal = 2.0;
+
+    private EaseFactor(Double easeFactor) {
+        if (easeFactor < minimum) {
+            this.easeFactor = minimum;
+            return;
+        }
+        if (easeFactor > maximum) {
+            this.easeFactor = maximum;
+            return;
+        }
+        this.easeFactor = easeFactor;
+    }
+
+    public static EaseFactor makeDefaultFactor() {
+        return new EaseFactor(defaultVal);
+    }
+}
