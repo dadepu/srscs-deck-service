913196b29559acf96682ed115bb88fdab982d768
dadepu <danielkoellgen@gmail.com>
2022-03-25 14:43:51 +0100

    feat(schedulerpreset): add LapseFactorModifier primitive

---
+++ src/main/java/de/danielkoellgen/srscsdeckservice/domain/schedulerpreset/domain/LapseFactorModifier.java
@@ -0,0 +1,41 @@
+package de.danielkoellgen.srscsdeckservice.domain.schedulerpreset.domain;
+
+import org.jetbrains.annotations.NotNull;
+
+public class LapseFactorModifier implements FactorModifier {
+
+    @NotNull
+    private final Double lapseFactorModifier;
+
+    private static final Double minimum = -0.75;
+
+    private static final Double maximum = 0.0;
+
+    private static final Double defaultVal = -0.2;
+
+    private LapseFactorModifier(@NotNull Double modifier) {
+        this.lapseFactorModifier = modifier;
+    }
+
+    public static LapseFactorModifier makeFromDefault() {
+        return new LapseFactorModifier(defaultVal);
+    }
+
+    public static LapseFactorModifier makeFromDouble(@NotNull Double modifier) {
+        return new LapseFactorModifier(modifier);
+    }
+
+    private static void validateOrThrow(@NotNull Double modifier) {
+        if (modifier < minimum) {
+            throw new IllegalArgumentException("Modifier may not be below " + minimum + ".");
+        }
+        if (modifier > maximum) {
+            throw new IllegalArgumentException("Modifier may not be above " + maximum + ".");
+        }
+    }
+
+    @Override
+    public @NotNull Double getFactorModifier() {
+        return lapseFactorModifier;
+    }
+}
