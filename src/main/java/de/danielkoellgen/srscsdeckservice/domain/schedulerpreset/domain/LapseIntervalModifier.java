2a5002a5d35c98b8f6a718292ad887dc565eb30d
dadepu <danielkoellgen@gmail.com>
2022-03-25 15:29:33 +0100

    refactor(schedulerpreset): rename and conform LapseIntervalModifier to IntervalModifier

---
+++ src/main/java/de/danielkoellgen/srscsdeckservice/domain/schedulerpreset/domain/LapseIntervalModifier.java
@@ -0,0 +1,42 @@
+package de.danielkoellgen.srscsdeckservice.domain.schedulerpreset.domain;
+
+import org.jetbrains.annotations.NotNull;
+
+public class LapseIntervalModifier implements IntervalModifier {
+
+    @NotNull
+    private final Double lapseIntervalModifier;
+
+    private static final Double minimum = 0.05;
+
+    private static final Double maximum = 0.95;
+
+    private static final Double defaultVal = 0.65;
+
+    private LapseIntervalModifier(@NotNull Double modifier) {
+        validateOrThrow(modifier);
+        this.lapseIntervalModifier = modifier;
+    }
+
+    public static LapseIntervalModifier makeDefaultFactor() {
+        return new LapseIntervalModifier(defaultVal);
+    }
+
+    public static LapseIntervalModifier makeFromDouble(@NotNull Double modifier) {
+        return new LapseIntervalModifier(modifier);
+    }
+
+    private static void validateOrThrow(@NotNull Double modifier) {
+        if (modifier < minimum) {
+            throw new IllegalArgumentException("Modifier may not be below " + minimum + ".");
+        }
+        if (modifier > maximum) {
+            throw new IllegalArgumentException("Modifier may not be above " + maximum + ".");
+        }
+    }
+
+    @Override
+    public @NotNull Double getIntervalModifier() {
+        return lapseIntervalModifier;
+    }
+}
