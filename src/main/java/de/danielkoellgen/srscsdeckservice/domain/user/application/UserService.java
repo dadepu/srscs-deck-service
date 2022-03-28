c49a3984c9c938db54a9b4860e65a81da9252be4
dadepu <danielkoellgen@gmail.com>
2022-03-28 18:35:50 +0200

    feat(user): update all EmbeddedUsers when User changes

--- src/main/java/de/danielkoellgen/srscsdeckservice/domain/user/application/UserService.java
+++ src/main/java/de/danielkoellgen/srscsdeckservice/domain/user/application/UserService.java
@@ -1,5 +1,9 @@
 package de.danielkoellgen.srscsdeckservice.domain.user.application;
 
+import de.danielkoellgen.srscsdeckservice.domain.deck.domain.Deck;
+import de.danielkoellgen.srscsdeckservice.domain.deck.repository.DeckRepository;
+import de.danielkoellgen.srscsdeckservice.domain.schedulerpreset.domain.SchedulerPreset;
+import de.danielkoellgen.srscsdeckservice.domain.schedulerpreset.repository.SchedulerPresetRepository;
 import de.danielkoellgen.srscsdeckservice.domain.user.domain.Username;
 import de.danielkoellgen.srscsdeckservice.domain.user.domain.User;
 import de.danielkoellgen.srscsdeckservice.domain.user.repository.UserRepository;
@@ -8,6 +12,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.UUID;
 
@@ -15,12 +20,17 @@ import java.util.UUID;
 public class UserService {
 
     private final UserRepository userRepository;
+    private final DeckRepository deckRepository;
+    private final SchedulerPresetRepository schedulerPresetRepository;
 
     private final Logger logger = LoggerFactory.getLogger(UserService.class);
 
     @Autowired
-    public UserService(UserRepository userRepository) {
+    public UserService(UserRepository userRepository, DeckRepository deckRepository,
+            SchedulerPresetRepository schedulerPresetRepository) {
         this.userRepository = userRepository;
+        this.deckRepository = deckRepository;
+        this.schedulerPresetRepository = schedulerPresetRepository;
     }
 
     public void addNewExternallyCreatedUser(UUID transactionId, UUID userId, Username username) {
@@ -40,6 +50,22 @@ public class UserService {
 
         logger.info("User '{}' renamed to '{}'. [tid={}, userId={}]",
                 oldName.getUsername(), user.getUsername().getUsername(), transactionId, userId);
+
+        List<Deck> userOwnedDecks = deckRepository.findDecksByEmbeddedUser_UserId(userId);
+        userOwnedDecks.forEach(deck -> deck.updateEmbeddedUser(user));
+        deckRepository.saveAll(userOwnedDecks);
+
+        logger.info("Updated {} Decks' EmbeddedUser from {}. [tid={}, userId={}]", userOwnedDecks.size(),
+                newUsername.getUsername(), transactionId, userId);
+
+        List<SchedulerPreset> userOwnedPresets = schedulerPresetRepository
+                .findSchedulerPresetsByEmbeddedUser_UserId(userId);
+        userOwnedPresets.forEach(preset -> preset.updateEmbeddedUser(user));
+        schedulerPresetRepository.saveAll(userOwnedPresets);
+
+        logger.info("Updated {} SchedulerPresets' EmbeddedUser from {}. [tid={}, userId={}]", userOwnedPresets.size(),
+                newUsername.getUsername(), transactionId, userId);
+
     }
 
     public void disableExternallyDisabledUser(UUID transactionId, UUID userId) throws NoSuchElementException  {
