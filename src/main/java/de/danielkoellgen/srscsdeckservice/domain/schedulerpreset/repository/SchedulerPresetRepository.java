dd7525e612552263e43ba7b8bf4d9c968cd66a97
dadepu <danielkoellgen@gmail.com>
2022-03-28 18:26:58 +0200

    feat(schedulerpreset): allow SchedulerPresets to be found by userId

--- src/main/java/de/danielkoellgen/srscsdeckservice/domain/schedulerpreset/repository/SchedulerPresetRepository.java
+++ src/main/java/de/danielkoellgen/srscsdeckservice/domain/schedulerpreset/repository/SchedulerPresetRepository.java
@@ -1,9 +1,13 @@
 package de.danielkoellgen.srscsdeckservice.domain.schedulerpreset.repository;
 
 import de.danielkoellgen.srscsdeckservice.domain.schedulerpreset.domain.SchedulerPreset;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface SchedulerPresetRepository extends CrudRepository<SchedulerPreset, UUID> {
+
+    List<SchedulerPreset> findSchedulerPresetsByEmbeddedUser_UserId(@NotNull UUID userId);
 }
