9ead8ffbe64c6413f1e4d1b0ac6346a9f7a5e89a
dadepu <danielkoellgen@gmail.com>
2022-03-28 18:20:52 +0200

    feat(deck): allow Decks to be found by User

--- src/main/java/de/danielkoellgen/srscsdeckservice/domain/deck/repository/DeckRepository.java
+++ src/main/java/de/danielkoellgen/srscsdeckservice/domain/deck/repository/DeckRepository.java
@@ -1,9 +1,13 @@
 package de.danielkoellgen.srscsdeckservice.domain.deck.repository;
 
 import de.danielkoellgen.srscsdeckservice.domain.deck.domain.Deck;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface DeckRepository extends CrudRepository<Deck, UUID> {
+
+    List<Deck> findDecksByEmbeddedUser_UserId(@NotNull UUID userId);
 }
