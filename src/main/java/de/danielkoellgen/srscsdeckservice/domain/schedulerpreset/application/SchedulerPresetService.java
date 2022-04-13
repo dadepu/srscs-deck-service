52a170ee0d13e93fef6dde82ed188f97b7e4dc5a
dadepu <danielkoellgen@gmail.com>
2022-04-13 06:09:19 +0200

    feat(schedulerPreset): allow to disable SchedulerPresets

---
+++ src/main/java/de/danielkoellgen/srscsdeckservice/domain/schedulerpreset/application/SchedulerPresetService.java
@@ -0,0 +1,33 @@
+package de.danielkoellgen.srscsdeckservice.domain.schedulerpreset.application;
+
+import de.danielkoellgen.srscsdeckservice.domain.schedulerpreset.domain.SchedulerPreset;
+import de.danielkoellgen.srscsdeckservice.domain.schedulerpreset.repository.SchedulerPresetRepository;
+import org.jetbrains.annotations.NotNull;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+public class SchedulerPresetService {
+
+    private final SchedulerPresetRepository schedulerPresetRepository;
+
+    private final Logger logger = LoggerFactory.getLogger(SchedulerPresetService.class);
+
+    @Autowired
+    public SchedulerPresetService(SchedulerPresetRepository schedulerPresetRepository) {
+        this.schedulerPresetRepository = schedulerPresetRepository;
+    }
+
+    public void disablePreset(@NotNull UUID transactionId, @NotNull UUID presetId) {
+        SchedulerPreset preset = schedulerPresetRepository.findById(presetId).get();
+        preset.disablePreset();
+        schedulerPresetRepository.save(preset);
+
+        logger.info("Preset {} disabled. [tid={}, presetId={}]",
+                preset.getPresetName().getName(), transactionId, presetId);
+    }
+}
