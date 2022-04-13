a678e0910aab5080ed84f8e9f22597e7ef4e83db
dadepu <danielkoellgen@gmail.com>
2022-04-13 06:20:59 +0200

    feat(schedulerPreset): allow to create transient default Presets

--- src/main/java/de/danielkoellgen/srscsdeckservice/domain/schedulerpreset/application/SchedulerPresetService.java
+++ src/main/java/de/danielkoellgen/srscsdeckservice/domain/schedulerpreset/application/SchedulerPresetService.java
@@ -1,7 +1,10 @@
 package de.danielkoellgen.srscsdeckservice.domain.schedulerpreset.application;
 
+import de.danielkoellgen.srscsdeckservice.domain.schedulerpreset.domain.PresetName;
 import de.danielkoellgen.srscsdeckservice.domain.schedulerpreset.domain.SchedulerPreset;
 import de.danielkoellgen.srscsdeckservice.domain.schedulerpreset.repository.SchedulerPresetRepository;
+import de.danielkoellgen.srscsdeckservice.domain.user.domain.User;
+import de.danielkoellgen.srscsdeckservice.domain.user.domain.Username;
 import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -20,6 +23,14 @@ public class SchedulerPresetService {
     @Autowired
     public SchedulerPresetService(SchedulerPresetRepository schedulerPresetRepository) {
         this.schedulerPresetRepository = schedulerPresetRepository;
+    }
+
+    public SchedulerPreset makeTransientDefault(@NotNull User user) {
+        try {
+            return new SchedulerPreset(new PresetName("default"), user);
+        } catch (Exception e) {
+            throw new RuntimeException("Preset-name invalid.");
+        }
     }
 
     public void disablePreset(@NotNull UUID transactionId, @NotNull UUID presetId) {
