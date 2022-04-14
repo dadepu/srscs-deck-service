f773c4f4c85fd3d3cb1ad1a9924c761c3db2b2f9
dadepu <danielkoellgen@gmail.com>
2022-04-14 07:58:48 +0200

    feat(events): add DeckRenamed Event

---
+++ src/main/java/de/danielkoellgen/srscsdeckservice/events/deck/DeckRenamed.java
@@ -0,0 +1,39 @@
+package de.danielkoellgen.srscsdeckservice.events.deck;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import de.danielkoellgen.srscsdeckservice.domain.domainprimitive.EventDateTime;
+import de.danielkoellgen.srscsdeckservice.events.AbstractProducerEvent;
+import de.danielkoellgen.srscsdeckservice.events.deck.dto.DeckCreatedDto;
+import de.danielkoellgen.srscsdeckservice.events.deck.dto.DeckRenamedDto;
+import org.jetbrains.annotations.NotNull;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+public class DeckRenamed extends AbstractProducerEvent {
+
+    @NotNull
+    private final DeckRenamedDto payloadDto;
+
+    public static final String eventName = "deck-renamed";
+
+    public static final Integer eventVersion = 1;
+
+    public static final String eventTopic = "cdc.decks-cards.0";
+
+    public DeckRenamed(@NotNull UUID transactionId, @NotNull DeckRenamedDto payloadDto) {
+        super(UUID.randomUUID(), transactionId, eventVersion, eventName, eventTopic,
+                EventDateTime.makeFromLocalDateTime(LocalDateTime.now()));
+        this.payloadDto = payloadDto;
+    }
+
+    @Override
+    public @NotNull String getSerializedContent() {
+        ObjectMapper objectMapper = new ObjectMapper().findAndRegisterModules();
+        try {
+            return objectMapper.writeValueAsString(payloadDto);
+        } catch (Exception e) {
+            throw new RuntimeException("ObjectMapper conversion failed.");
+        }
+    }
+}
