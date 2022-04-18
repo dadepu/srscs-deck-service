ba9f326e49da7ec60cca441b6d0b6f7baee1f901
dadepu <danielkoellgen@gmail.com>
2022-04-18 13:07:29 +0200

    test(commands): should clone Decks and Cards when receiving CloneDeckCmd

--- src/test/java/de/danielkoellgen/srscsdeckservice/commands/deckcards/KafkaDeckCardsCommandConsumerIntegrationTest.java
+++ src/test/java/de/danielkoellgen/srscsdeckservice/commands/deckcards/KafkaDeckCardsCommandConsumerIntegrationTest.java
@@ -1,11 +1,18 @@
 package de.danielkoellgen.srscsdeckservice.commands.deckcards;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import de.danielkoellgen.srscsdeckservice.commands.deckcards.dto.CloneDeckDto;
 import de.danielkoellgen.srscsdeckservice.commands.deckcards.dto.CreateDeckDto;
 import de.danielkoellgen.srscsdeckservice.core.converter.ProducerEventToConsumerRecordConverter;
+import de.danielkoellgen.srscsdeckservice.core.events.producer.CloneDeckCmd;
 import de.danielkoellgen.srscsdeckservice.core.events.producer.CreateDeckCmd;
+import de.danielkoellgen.srscsdeckservice.domain.card.application.CardService;
+import de.danielkoellgen.srscsdeckservice.domain.card.domain.DefaultCard;
 import de.danielkoellgen.srscsdeckservice.domain.card.repository.CardRepository;
+import de.danielkoellgen.srscsdeckservice.domain.card.repository.DefaultCardRepository;
+import de.danielkoellgen.srscsdeckservice.domain.deck.application.DeckService;
 import de.danielkoellgen.srscsdeckservice.domain.deck.domain.Deck;
+import de.danielkoellgen.srscsdeckservice.domain.deck.domain.DeckName;
 import de.danielkoellgen.srscsdeckservice.domain.deck.repository.DeckRepository;
 import de.danielkoellgen.srscsdeckservice.domain.user.application.UserService;
 import de.danielkoellgen.srscsdeckservice.domain.user.domain.User;
@@ -17,6 +24,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.List;
 import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -27,24 +35,33 @@ public class KafkaDeckCardsCommandConsumerIntegrationTest {
     private final KafkaDeckCardsCommandConsumer kafkaDeckCardsCommandConsumer;
 
     private final UserService userService;
+    private final DeckService deckService;
+    private final CardService cardService;
 
     private final UserRepository userRepository;
     private final DeckRepository deckRepository;
     private final CardRepository cardRepository;
+    private final DefaultCardRepository defaultCardRepository;
 
     private final ProducerEventToConsumerRecordConverter converterToConsumerRecord;
 
     private User user;
+    private Deck deck;
+    private List<DefaultCard> cards;
 
     @Autowired
     public KafkaDeckCardsCommandConsumerIntegrationTest(KafkaDeckCardsCommandConsumer kafkaDeckCardsCommandConsumer,
-            UserService userService, UserRepository userRepository, DeckRepository deckRepository,
-            CardRepository cardRepository, ProducerEventToConsumerRecordConverter producerEventToConsumerRecordConverter) {
+            UserService userService, DeckService deckService, CardService cardService, UserRepository userRepository,
+            DeckRepository deckRepository, CardRepository cardRepository, DefaultCardRepository defaultCardRepository,
+            ProducerEventToConsumerRecordConverter producerEventToConsumerRecordConverter) {
         this.kafkaDeckCardsCommandConsumer = kafkaDeckCardsCommandConsumer;
         this.userService = userService;
+        this.deckService = deckService;
+        this.cardService = cardService;
         this.userRepository = userRepository;
         this.deckRepository = deckRepository;
         this.cardRepository = cardRepository;
+        this.defaultCardRepository = defaultCardRepository;
         this.converterToConsumerRecord = producerEventToConsumerRecordConverter;
     }
 
@@ -53,11 +70,19 @@ public class KafkaDeckCardsCommandConsumerIntegrationTest {
         user = userService.addNewExternallyCreatedUser(
                 UUID.randomUUID(), UUID.randomUUID(), new Username("dadepu")
         );
+        deck = deckService.createNewDeck(
+                UUID.randomUUID(), user.getUserId(), new DeckName("DECK1")
+        );
+        cards = List.of(
+                cardService.createDefaultCard(UUID.randomUUID(), deck.getDeckId(), null, null, null),
+                cardService.createDefaultCard(UUID.randomUUID(), deck.getDeckId(), null, null, null),
+                cardService.createDefaultCard(UUID.randomUUID(), deck.getDeckId(), null, null, null)
+        );
     }
 
     @AfterEach
     public void cleanUp() {
-        cardRepository.deleteAll();
+//        cardRepository.deleteAll();
         deckRepository.deleteAll();
         userRepository.deleteAll();
     }
@@ -77,5 +102,28 @@ public class KafkaDeckCardsCommandConsumerIntegrationTest {
         // then
         assertThat(deckRepository.findDecksByEmbeddedUser_UserId(user.getUserId()))
                 .hasSize(1);
+    }
+
+    @Test
+    public void shouldCloneDeckWhenReceivingCloneDeckCommand() throws Exception {
+        // given
+        CloneDeckCmd cloneDeckCmd = new CloneDeckCmd(UUID.randomUUID(), new CloneDeckDto(
+                deck.getDeckId(), user.getUserId(), "target"
+        ));
+
+        // when
+        kafkaDeckCardsCommandConsumer.receive(
+                converterToConsumerRecord.convert(cloneDeckCmd)
+        );
+
+        // then
+        Deck clonedDeck = deckRepository.findDecksByEmbeddedUser_UserId(user.getUserId()).stream()
+                .filter(deck -> deck.getDeckName().getName().equals("target"))
+                .toList().get(0);
+
+        // and then
+        List<DefaultCard> clonedCards = defaultCardRepository.findAllByEmbeddedDeck_DeckId(clonedDeck.getDeckId());
+        assertThat(clonedCards)
+                .hasSize(3);
     }
 }
